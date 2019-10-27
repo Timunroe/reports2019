6b7f19071d55af50f0709561f0ec0c0db2677270
Tim Munroe <munroe.tim@gmail.com>
2019-10-26 23:06:24 -0400

    Oct.26 late

--- daily.py
+++ daily.py
@@ -7,3 +7,12 @@ import pprint
 import utils as u
 import config as c
 
+
+'''
+Read yesterday's file ...
+Download yesterday's site stats, drop in daily folder
+What safeguards to make sure it's the right file?
+filename = spectator-site-yesterday-thespec-com-any.csv
+> get date of stats, compare with last date in master file?
+
+'''
